8ce7eb30601f83422df1cb6db25ea40f8ff2e236
adrii4 <adri.regalado@live.com>
2020-02-07 15:43:51 -0600

    Finished Milestone 1

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -4,7 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        EditText myTextField = (EditText) findViewById(R.id.editText);
+        String str = myTextField.getText().toString();
+        goToActivity2(str);
+
+        // Toast.makeText( MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void goToActivity2(String s) {
+
+        Intent intent = new Intent ( this, Main2Activity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
