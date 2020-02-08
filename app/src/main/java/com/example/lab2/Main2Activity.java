f89c5abc14f884ee92fcdfa1c8c6db37fe444868
adrii4 <adri.regalado@live.com>
2020-02-08 17:26:39 -0600

    Finished Milestone 2

--- app/src/main/java/com/example/lab2/Main2Activity.java
+++ app/src/main/java/com/example/lab2/Main2Activity.java
@@ -3,12 +3,15 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+
 import android.os.Bundle;
+
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
     TextView textView2;
+    TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +19,11 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
         Intent intent = getIntent();
         String str = intent.getStringExtra("message");
-        textView2.setText("Hello " + str);
+        textView2.setText(str + " dollars in pounds is...");
+        String str2 = intent.getStringExtra("result");
+        textView3.setText("£ " + str2);
     }
 }
--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -4,26 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+
 import android.view.View;
 
 import android.widget.EditText;
+
 import android.widget.Toast;
+
 import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
 
         EditText myTextField = (EditText) findViewById(R.id.editText);
         String str = myTextField.getText().toString();
-        goToActivity2(str);
+        double value = Double.parseDouble(str);
+        goToActivity2(value);
 
         // Toast.makeText( MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
     }
 
-    public void goToActivity2(String s) {
+    public void goToActivity2(double value) {
 
         Intent intent = new Intent ( this, Main2Activity.class);
-        intent.putExtra("message", s);
+        intent.putExtra("message",  value + "");
+        double conversion = value * 0.77;
+        double rounded = Math.round(conversion * 100.0) / 100.0;
+        intent.putExtra("result", rounded + "");
         startActivity(intent);
     }
 
